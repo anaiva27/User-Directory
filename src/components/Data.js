769bf606f2857f5be933f8ad31c7c3192f577637
Anastasia <nastia.iv.91@gmail.com>
2021-02-22 22:58:30 -0800

    user data is loading, search and sort functions working

---
+++ src/components/Data.js
@@ -0,0 +1,19 @@
+export default function Data(props) {
+  return (
+    <>
+      {props.employees.map((result) => (
+        <tr key={result.login.uuid}>
+          <td>
+            <img src={result.picture.medium} alt={result.name.first} />
+          </td>
+          <td>
+            {result.name.first} {result.name.last}
+          </td>
+          <td>{result.phone}</td>
+          <td>{result.email}</td>
+          <td>{result.dob.date}</td>
+        </tr>
+      ))}
+    </>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import "../styles/header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Employee Directory</h1>
+      <p>
+        Use the search box to narrow your
+        results or click the sort button to sort by name.
+      </p>
+    </header>
+  );
+}
+
